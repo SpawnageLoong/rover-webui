6da59cd3d4455cdd3169bdcee2ad385f63d31b1a
Richard Loong <richardloongcj@gmail.com>
2023-09-13 22:21:34 +0800

    Remove TS, Basic ROS interface added

---
+++ src/lib/stores.js
@@ -0,0 +1,8 @@
+import { writable } from "svelte/store";
+
+export const pwm0_store = writable(0);
+export const pwm1_store = writable(0);
+export const pwm2_store = writable(0);
+export const pwm3_store = writable(0);
+export const pwm4_store = writable(0);
+export const pwm5_store = writable(0);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,62 @@
-<svelte:head>
-    <script src="https://cdn.jsdelivr.net/npm/roslib@1/build/roslib.js"></script>
-</svelte:head>
+<script lang="js">
+  import ROSLIB from 'roslib';
+  import { pwm0_store } from '$lib/stores.js';
+
+  let pwm0 = 0;
+
+  pwm0_store.subscribe(value => {
+    pwm0 = value;
+  });
+
+  // Connecting to ROS
+    // -----------------
+  
+    var ros = new ROSLIB.Ros({
+      url : 'ws://192.168.1.168:9090'
+    });
+  
+    ros.on('connection', function() {
+      console.log('Connected to websocket server.');
+    });
+  
+    ros.on('error', function(error) {
+      console.log('Error connecting to websocket server: ', error);
+    });
+  
+    ros.on('close', function() {
+      console.log('Connection to websocket server closed.');
+    });
+  
+    // Subscribing to a Topic
+    // ----------------------
+  
+    var listener = new ROSLIB.Topic({
+      ros : ros,
+      name : '/listener',
+      messageType : 'std_msgs/String'
+    });
+  
+    listener.subscribe(function(message) {
+      console.log('Received message on ' + listener.name + ': ' + message.data);
+    });
+  
+    var pwm0_subscriber = new ROSLIB.Topic({
+      ros : ros,
+      name : '/pwm0',
+      messageType : 'std_msgs/Int32'
+    });
+
+    pwm0_subscriber.subscribe(function(message) {
+      console.log('Received message on ' + pwm0_subscriber.name + ': ' + message.data);
+      pwm0_store.set(message.data);
+    });
+
+</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Simple roslib Example</h1>
+<p>Check your Web Console for output.</p>
+
+<p>PWM 0 Value: {pwm0}</p>
