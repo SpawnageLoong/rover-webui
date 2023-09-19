866711686fbdfdd320b9011c215a923fdbdfbac4
SpawnageLoong <richardloongcj@gmail.com>
2023-09-19 14:29:45 +0800

    Add preline

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <script src="/node_modules/preline/dist/preline.js"></script>
+</svelte:head>
+
 <script>
     import "../app.css";
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    'node_modules/preline/dist/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
-
