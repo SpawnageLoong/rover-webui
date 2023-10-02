d5b7e68b9beee9216b47467ad5856887f3c5c52f
Richard Loong <richardloongcj@gmail.com>
2023-10-02 14:54:04 +0800

    Add skeleton navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
     import "../app.css";
+    import Navbar from "$lib/components/navbar.svelte";
 </script>
   
-<slot />
+<html lang="en">
+    <Navbar />
+    <div class="mt-12 mb-16 w-full items-center bg-background min-h-[calc(100vh-112px)]">
+      <slot />
+    </div>
+</html>
