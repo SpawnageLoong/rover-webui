004693786c59297faae52d28550e26b68bc3db49
Richard Loong <richardloongcj@gmail.com>
2023-10-02 13:01:11 +0800

    Add sensor stores

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -7,5 +7,26 @@ export const pwm3_store = writable(0);
 export const pwm4_store = writable(0);
 export const pwm5_store = writable(0);
 
-export const vel_linear = writable(0);
+export const vel_linear  = writable(0);
 export const vel_angular = writable(0);
+
+export const ext_temp0 = writable(0);
+export const int_temp0 = writable(0);
+export const int_temp1 = writable(0);
+export const int_temp2 = writable(0);
+export const int_hum0  = writable(0);
+export const int_hum1  = writable(0);
+export const int_hum2  = writable(0);
+export const int_pres0 = writable(0);
+export const alt0      = writable(0);
+
+export const accel_x  = writable(0);
+export const accel_y  = writable(0);
+export const accel_z  = writable(0);
+export const gyro_x   = writable(0);
+export const gyro_y   = writable(0);
+export const gyro_z   = writable(0);
+export const mag_x    = writable(0);
+export const mag_y    = writable(0);
+export const mag_z    = writable(0);
+export const imu_temp = writable(0);
