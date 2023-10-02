a75a87f1200ee38b9cb96ca09490ba98828826e6
Richard Loong <richardloongcj@gmail.com>
2023-10-02 14:54:30 +0800

    Add skeleton navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,21 @@
+<script lang=js>
+    let dropdown = false;
+
+    function handleDropdownOpen() {
+        dropdown = true;
+        document.body.addEventListener('click', handleDropdownClose);
+    }
+
+    function handleDropdownClose() {
+        dropdown = false;
+        document.body.removeEventListener('click', handleDropdownClose);
+    }
+</script>
+
+<div class="w-full h-12 fixed top-0 bg-gray-700 z-[100]">
+    <a href="/">
+        <div class="absolute left-0 m-1 p-2 rounded-md text-center text-white hover:bg-gray-600">
+            Endeavour OS
+        </div>
+    </a>
+</div>
