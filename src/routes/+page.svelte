5ff51ca20643b2379898a0349c2bfc7ede4c43f9
Richard Loong <richardloongcj@gmail.com>
2023-10-03 19:04:04 +0800

    Add video stream

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,48 +14,61 @@
   })
 </script>
 
-<h1>Simple UI</h1>
-Host Name: {$host}
-<br>
-
-Target Velocities:
-<ul>
-  <li>Linear: {$vel_linear}</li>
-  <li>Angular: {$vel_angular}</li>
-</ul>
-<br>
-
-PWM Values:
-<ul>
-  <li>PWM 0: {$pwm0}</li>
-  <li>PWM 1: {$pwm1}</li>
-  <li>PWM 2: {$pwm2}</li>
-  <li>PWM 3: {$pwm3}</li>
-  <li>PWM 4: {$pwm4}</li>
-  <li>PWM 5: {$pwm5}</li>
-</ul>
-<br>
-
-Telemetry:
-<ul>
-  <li>External Temperature: {$ext_temp0}</li>
-  <li>Internal Temperature 0: {$int_temp0}</li>
-  <li>Internal Temperature 1: {$int_temp1}</li>
-  <li>Internal Temperature 2: {$int_temp2}</li>
-  <li>Internal Temperature 3: {$imu_temp}</li>
-  <li>Internal Humidity 0: {$int_hum0}</li>
-  <li>Internal Humidity 1: {$int_hum1}</li>
-  <li>Internal Humidity 2: {$int_hum2}</li>
-  <li>Internal Pressure 0: {$int_pres0}</li>
-  <li>Altitude 0: {$alt0}</li>
+<div class="p-4">
+  Host Name: {$host}
   <br>
-  <li>Acceleration X: {$accel_x}</li>
-  <li>Acceleration Y: {$accel_y}</li>
-  <li>Acceleration Z: {$accel_z}</li>
-  <li>Gyro X: {$gyro_x}</li>
-  <li>Gyro Y: {$gyro_y}</li>
-  <li>Gyro Z: {$gyro_z}</li>
-  <li>Magnetometer X: {$mag_x}</li>
-  <li>Magnetometer Y: {$mag_y}</li>
-  <li>Magnetometer Z: {$mag_z}</li>
-</ul>
+</div>
+
+<div class="flex m-4">
+  <div class="flex-auto">
+    <img
+      src="http://{$host}:8081"
+      width="960"
+      height="540"
+      alt="Front camera stream">
+  </div>
+
+  <div class="flex-auto">
+    Target Velocities:
+    <ul>
+      <li>Linear: {$vel_linear}</li>
+      <li>Angular: {$vel_angular}</li>
+    </ul>
+    <br>
+  
+    PWM Values:
+    <ul>
+      <li>PWM 0: {$pwm0}</li>
+      <li>PWM 1: {$pwm1}</li>
+      <li>PWM 2: {$pwm2}</li>
+      <li>PWM 3: {$pwm3}</li>
+      <li>PWM 4: {$pwm4}</li>
+      <li>PWM 5: {$pwm5}</li>
+    </ul>
+    <br>
+  
+    Telemetry:
+    <ul>
+      <li>External Temperature: {$ext_temp0}</li>
+      <li>Internal Temperature 0: {$int_temp0}</li>
+      <li>Internal Temperature 1: {$int_temp1}</li>
+      <li>Internal Temperature 2: {$int_temp2}</li>
+      <li>Internal Temperature 3: {$imu_temp}</li>
+      <li>Internal Humidity 0: {$int_hum0}</li>
+      <li>Internal Humidity 1: {$int_hum1}</li>
+      <li>Internal Humidity 2: {$int_hum2}</li>
+      <li>Internal Pressure 0: {$int_pres0}</li>
+      <li>Altitude 0: {$alt0}</li>
+      <br>
+      <li>Acceleration X: {$accel_x}</li>
+      <li>Acceleration Y: {$accel_y}</li>
+      <li>Acceleration Z: {$accel_z}</li>
+      <li>Gyro X: {$gyro_x}</li>
+      <li>Gyro Y: {$gyro_y}</li>
+      <li>Gyro Z: {$gyro_z}</li>
+      <li>Magnetometer X: {$mag_x}</li>
+      <li>Magnetometer Y: {$mag_y}</li>
+      <li>Magnetometer Z: {$mag_z}</li>
+    </ul>
+  </div>
+</div>
