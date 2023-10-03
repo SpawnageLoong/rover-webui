705301fd6ea304adf9e7969f093df79a79747117
Richard Loong <richardloongcj@gmail.com>
2023-10-04 01:55:34 +0800

    Add white as default text colour

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
   
 <html lang="en">
     <Navbar />
-    <div class="mt-12 w-full items-center bg-gray-800 min-h-[calc(100vh-48px)]">
+    <div class="mt-12 w-full items-center bg-gray-800 min-h-[calc(100vh-48px)] text-white">
       <slot />
     </div>
 </html>
