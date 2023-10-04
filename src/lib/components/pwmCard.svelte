a694acf80641bea15c8b5c0377a6f63eb9bd6c2e
Richard Loong <richardloongcj@gmail.com>
2023-10-04 09:45:23 +0800

    Add pwmCard component

---
+++ src/lib/components/pwmCard.svelte
@@ -0,0 +1,12 @@
+<script lang="js">
+    export let pwmVal;
+</script>
+
+<div class="flex bg-gray-600 rounded-xl shadow-lg p-4">
+    <!-- Throttle Bar -->
+    <div class="flex flex-col items-center">
+        <div class="h-24 w-5 overflow-hidden rounded-md bg-slate-400">
+            <div class="h-full bg-green-800" style="height: {pwmVal / 255}"></div>
+        </div>
+    </div>
+</div>
