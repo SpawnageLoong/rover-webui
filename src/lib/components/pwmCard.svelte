cd4f6c131f1d7347fbb3951eab334ce7f4ed2cf1
Richard Loong <richardloongcj@gmail.com>
2023-10-15 13:05:42 +0800

    Add PWM Card and test page

--- src/lib/components/pwmCard.svelte
+++ src/lib/components/pwmCard.svelte
@@ -1,12 +1,34 @@
 <script lang="js">
     export let pwmVal;
+    var circumference = 2 * Math.PI * 30;
 </script>
 
-<div class="flex bg-gray-600 rounded-xl shadow-lg p-4">
-    <!-- Throttle Bar -->
-    <div class="flex flex-col items-center">
-        <div class="h-24 w-5 overflow-hidden rounded-md bg-slate-400">
-            <div class="h-full bg-green-800" style="height: {pwmVal / 255}"></div>
-        </div>
-    </div>
+<div class="flex flex-col items-center bg-gray-600 rounded-xl shadow-lg p-4 w-28">
+    <svg class="w-20 h-20">
+        <circle
+            class="text-gray-300"
+            stroke-width="5"
+            stroke="currentColor"
+            fill="transparent"
+            r="30"
+            cx="40"
+            cy="40"
+        />
+        <circle
+            class="text-blue-500"
+            stroke-width="5"
+            stroke-dasharray="{circumference}"
+            stroke-dashoffset="{circumference - pwmVal / 255 * circumference}"
+            stroke-linecap="round"
+            stroke="currentColor"
+            fill="transparent"
+            r="30"
+            cx="40"
+            cy="40"
+        />
+    </svg>
+    <span class="absolute top-[88px] text-xl text-white">
+        {Math.round(pwmVal / 255 * 100)}%
+    </span>
+    <span class="text-xl text-white">{pwmVal}</span>
 </div>
---
+++ src/routes/componenttest/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="js">
+	import PwmCard from '$lib/components/pwmCard.svelte';
+    var pwmVal_test = 120;
+</script>
+
+<PwmCard pwmVal={pwmVal_test} />
