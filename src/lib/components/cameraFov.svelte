fb8f8bfa2e7a17ef1e04c6aa30e0602052e2b781
SpawnageLoong <richardloongcj@gmail.com>
2023-10-17 10:37:25 +0800

    Add backing circle, fill color and variable tf

--- src/lib/components/cameraFov.svelte
+++ src/lib/components/cameraFov.svelte
@@ -6,15 +6,24 @@
   <div class="text-xl flex flex-col basis-2/4">
     Horizontal FoV:
     <svg width="200" height="200">
+      <circle
+        cx="100"
+        cy="100"
+        r="100"
+        stroke="white"
+        stroke-width="2"
+        fill=transparent
+        stroke-dasharray="5, 5" />
       <path
         d="M 100 100
-            L 13.4 150
-            A 100 100 0 0 0 186.6 150
-            L 100 100"
+           L 13.4 150
+           A 100 100 0 0 0 186.6 150
+           L 100 100"
         stroke="red"
         stroke-width="2"
-        fill="none"
-        transform="rotate(180 100 100)">
+        fill="red"
+        fill-opacity="0.2"
+        transform="rotate({180 + $camera_pan} 100 100)"/>
     </svg>
   </div>
       
