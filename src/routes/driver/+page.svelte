d969932fb79def7a6d20efbb0dddbafabf6f2526
SpawnageLoong <richardloongcj@gmail.com>
2023-11-08 17:06:02 +0800

    Update driver view UI

--- src/routes/driver/+page.svelte
+++ src/routes/driver/+page.svelte
@@ -38,48 +38,25 @@
 
 </script>
 
-<div class="flex m-4 pt-4 gap-4">
-  <div class="basis-2/4 flex flex-col gap-4">
-    <CameraCard />
-    <InputSelector />
+<div class="flex flex-col m-4 pt-4 gap-4">
+  <div class="flex gap-4 w-full">
+    <div class="self-center basis-[30%]"><CameraCard /></div>
+    <div class="basis-2/5"><CameraCard /></div>
+    <div class="self-center basis-[30%]"><CameraCard /></div>
   </div>
 
   <div class="basis-2/4 flex gap-4">
+    <InputSelector />
     <div class="flex flex-col gap-4">
       <div class="bg-gray-600 rounded-xl shadow-lg px-4 py-2 text-xl">
-        Target Velocities:
-        <ul class="text-base">
-          <li>Linear: {Math.round($vel_linear*1000) / 1000}</li>
-          <li>Angular: {Math.round($vel_angular*1000) / 1000  * ($invert_controls ? -1 : 1)}</li>
-        </ul>
-      </div>
-
-      <div class="bg-gray-600 rounded-xl shadow-lg px-4 py-2 text-xl">
-        PWM Values:
-        <div class="grid grid-cols-2 grid-rows-3 gap-4 py-4">
+        <div class="grid grid-cols-2 grid-rows-1 gap-4 py-4">
           <div class="text-center col-start-1 row-start-1">
-            #1
+            Left
             <PwmCard pwmVal={$pwm0} />
           </div>
-          <div class="text-center col-start-1 row-start-2">
-            #2
-            <PwmCard pwmVal={$pwm1} />
-          </div>
-          <div class="text-center col-start-1 row-start-3">
-            #3
-            <PwmCard pwmVal={$pwm2} />
-          </div>
           <div class="text-center col-start-2 row-start-1">
-            #4
+            Right
             <PwmCard pwmVal={$pwm3} />
-          </div>
-          <div class="text-center col-start-2 row-start-2">
-            #5
-            <PwmCard pwmVal={$pwm4} />
-          </div>
-          <div class="text-center col-start-2 row-start-3">
-            #6
-            <PwmCard pwmVal={$pwm5} />
           </div>
         </div>
       </div>
@@ -87,35 +64,6 @@
 
     <div class="flex-auto flex flex-col gap-4">
       <CameraFov />
-
-      <div class="bg-gray-600 rounded-xl shadow-lg px-4 py-2">
-        Telemetry:
-        <ul>
-          <li>External Temperature: {$ext_temp0}</li>
-          <li>Internal Temperature 0: {$int_temp0}</li>
-          <li>Internal Temperature 1: {$int_temp1}</li>
-          <li>Internal Temperature 2: {$int_temp2}</li>
-          <li>Internal Temperature 3: {$imu_temp}</li>
-          <li>Internal Humidity 0: {$int_hum0}</li>
-          <li>Internal Humidity 1: {$int_hum1}</li>
-          <li>Internal Humidity 2: {$int_hum2}</li>
-          <li>Internal Pressure 0: {$int_pres0}</li>
-          <li>Altitude 0: {$alt0}</li>
-        </ul>
-      </div>
-      <div class="bg-gray-600 rounded-xl shadow-lg px-4 py-2">
-        <ul>
-          <li>Acceleration X: {$accel_x}</li>
-          <li>Acceleration Y: {$accel_y}</li>
-          <li>Acceleration Z: {$accel_z}</li>
-          <li>Gyro X: {$gyro_x}</li>
-          <li>Gyro Y: {$gyro_y}</li>
-          <li>Gyro Z: {$gyro_z}</li>
-          <li>Magnetometer X: {$mag_x}</li>
-          <li>Magnetometer Y: {$mag_y}</li>
-          <li>Magnetometer Z: {$mag_z}</li>
-        </ul>
-      </div>
     </div>
   </div>
 </div>
